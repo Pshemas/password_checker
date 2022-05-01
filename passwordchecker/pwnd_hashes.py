3ae6a4c8f71f5e1f99aa15a397332ce325c72a4c
Przemek Jeske <jeskenis@gmail.com>
2022-05-01 14:03:28 +0200

    first commit

--- passwordchecker/pwnd_hashes.py
+++ passwordchecker/pwnd_hashes.py
@@ -2,7 +2,7 @@ from requests import get
 
 
 class SimilarPwndHashedPasswords:
-    """create list of hashes similar to the one provided"""
+    """create container for a list of pwnd hashes similar to the one provided"""
 
     def __init__(self, five_hash_digits: str) -> None:
         self.similar = self.get_similar(five_hash_digits)
